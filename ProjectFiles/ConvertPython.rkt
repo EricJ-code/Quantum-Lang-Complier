b5fbdb556eec1183ca95b8bc8482228cbb059a7f
Tyler Thompson <tsquaredtech0630@gmail.com>
2024-04-21 16:51:27 -0400

    Update ConvertPython.rkt

--- ProjectFiles/ConvertPython.rkt
+++ ProjectFiles/ConvertPython.rkt
@@ -1,18 +1,28 @@
 #lang racket
 
+; Reads a file and returns it as a list of lines that is an s-expression.
 (define (read-file filename)
   (with-input-from-file filename
     (lambda ()
-      (let loop ((line (read-line)))
-        (cond
-          [(eof-object? line) '()]
-          [else (cons line (loop (read-line)))])))))
+      (let loop ((lines '()))
+        (let ((line (read-line)))
+          (if (eof-object? line)
+              (reverse lines)
+              (loop (cons line lines))))))))
 
+; Writes a string to python file.
 (define (write-to-py-file filename python_code)
   (with-output-to-file filename
     (lambda ()
       (display python_code))))
 
-(define lines (read-file "example.schr"))
+; Function that runs the script.
+(define (main input-file output-file)
+  (let ((lines (read-file input-file)))
+    (write-to-py-file output-file (string-join lines "\n"))))
 
-(write-to-py-file "" (string-join lines "\n"))
+; Parse to command line.
+(define input-file (vector-ref (current-command-line-arguments) 0))
+(define output-file (vector-ref (current-command-line-arguments) 1))
+
+(main input-file output-file)
