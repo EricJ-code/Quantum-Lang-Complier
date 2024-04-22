3641c31b2c451be6364d452c53dde4ba29efaf39
yash-amin9 <70327111+yash-amin9@users.noreply.github.com>
2024-04-21 20:28:54 -0400

    Update line parser alt.rkt
    Updated to be able to accept any name for the gate variable

--- ProjectFiles/line parser alt.rkt
+++ ProjectFiles/line parser alt.rkt
@@ -2,29 +2,23 @@
 
 (define (parse-line line)
   (match (string-split line " ")
-    ;; Match qubit initializations
     [(list "|0>" var-name)
-     (format "~a = qubit(0)" var-name)]
+     (format "~a = qubit(0)\n" var-name)]
     [(list "|1>" var-name)
-     (format "~a = qubit(1)" var-name)]
-    ;; Match default gates calls
-    [(list "g.h" var-name)
-     (format "g.default_gates('h', ~a)" var-name)]
-    [(list "g.x" var-name)
-     (format "g.default_gates('x', ~a)" var-name)]
-    [(list "g.y" var-name)
-     (format "g.default_gates('y', ~a)" var-name)]
-    [(list "g.z" var-name)
-     (format "g.default_gates('z', ~a)" var-name)]
-    ;; Handle unrecognized patterns
-    [_
-     (error "Unrecognized pattern")]))
+     (format "~a = qubit(1)\n" var-name)]
+    ; Handle different variable names for gate operations
+    [(list (regexp #rx"([a-zA-Z0-9_]+).(h|x|y|z)") var-name)
+     (define gate (second (regexp-match #rx"([a-zA-Z0-9_]+).(h|x|y|z)" (first (string-split line " ")))))
+     (define operation (third (regexp-match #rx"([a-zA-Z0-9_]+).(h|x|y|z)" (first (string-split line " ")))))
+     (format "~a.default_gates('~a', ~a)\n" gate operation var-name)]
+    [else
+     ""])) ; Return an empty string if no pattern matches
 
 ;; Example usage:
 (parse-line "|0> qubit1")
 (parse-line "|1> qubit1")
-(parse-line "g.h qubit1")
-(parse-line "g.x qubit2")
-(parse-line "g.y qubit3")
+(parse-line "fuck.h qubit1")
+(parse-line "this.x qubit2")
+(parse-line "shit.y qubit3")
 (parse-line "g.z qubit4")
 (parse-line "observe")
